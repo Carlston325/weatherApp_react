235a24dd126c15f2e115b534288631ae40094916
Carlston325 <130921730+Carlston325@users.noreply.github.com>
2024-06-09 22:41:13 +0100

    my update

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
-  base: "https://api.openweathermap.org/data/2.5/",
+  key: "bae09d4018e644968e46ed944b1ca373",
+  base: "https://api.weatherbit.io/v2.0/current",
 };
